bbcc3c333c04611101606e26c0ac9094ed7d8090
appsquilla <subhajit.webdev@gmail.com>
2021-07-16 02:54:06 +0530

    2021 version of site, developing

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
+          <Route path='/' component={Home} />
           <Route path='/home' component={Home} />
           <Route path='/professional-consultation'  component={Consultation} />
           <Route path='/services'  component={Services} />
