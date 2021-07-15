ccbf0c351ff3efaa48c6ef965f9edeff7ea9a381
appsquilla <subhajit.webdev@gmail.com>
2021-07-16 03:40:37 +0530

    2021 version of site,still  developing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Consultation from './pages/Consultation/Consultation'
 import Services from './pages/Services/Services'
@@ -9,16 +9,14 @@ import GetQuote from './pages/Services/GetQuote';
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
+      
           <Route exact path='/' component={Home} />
           <Route exact path='/home' component={Home} />
           <Route path='/professional-consultation'  component={Consultation} />
           <Route path='/services'  component={Services} />
           <Route path='/get-quote' component={GetQuote} />
           <Route path='/ask-for-demo' component={GetQuote} />
-        </Switch>
-      </Router>
+        
     </div>
   );
 }
